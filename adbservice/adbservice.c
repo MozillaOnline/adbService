06ed1f13366ef06526d0a685c2d376f76d18d1a9
dxue <dxue@mozilla.com>
2013-01-25 09:42:36 +0800

    add adb bin

--- adbservice/adbservice.c
+++ adbservice/adbservice.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifndef XP_LINUX
+#include <windows.h>
+#endif
+
 #define BUFFER_SIZE 1024 
 #define CMD_SIZE 128
 #define LOCAL_PORT 10010
 #define REMOTE_PORT 10010
 
-#ifndef XP_LINUX
+#ifdef XP_WIN
 __declspec(dllexport) 
 #endif
 int findDevice()
@@ -18,10 +22,23 @@ int findDevice()
 	int numread = 0;
 	char buffer[BUFFER_SIZE] = {0};
 	char *pb = 0;
-	char cmd[CMD_SIZE] = "adb devices > ";
+	char cmd[CMD_SIZE] = {0};
 	int ret = 0;
-	strcat(cmd, logname); 
-	ret = system(cmd);
+	
+#ifdef XP_WIN
+
+
+#else
+	#ifdef XP_MAC
+		strcpy(cmd, "./adb-mac devices > "); 
+		strcat(cmd, logname); 
+		ret = system(cmd);
+	#else
+		strcpy(cmd, "./adb-linux devices > "); 
+		strcat(cmd, logname); 
+		ret = system(cmd);
+	#endif
+#endif
 	if(ret)
 		return 0;
 	if( (fp = fopen(logname,"r")) == NULL )
@@ -50,11 +67,21 @@ __declspec(dllexport)
 #endif
 int setupDevice()
 {
-	char *logname ="adbsetup.log";
-        char cmd[CMD_SIZE] = {0};
+    char cmd[CMD_SIZE] = {0};
 	int ret = 0;
-	sprintf(cmd, "adb forward tcp:%d tcp:%d > %s ", LOCAL_PORT,REMOTE_PORT,logname);
-        ret = system(cmd);
+	
+#ifdef XP_WIN
+
+
+#else
+	#ifdef XP_MAC
+		sprintf(cmd, "./adb-mac forward tcp:%d tcp:%d", LOCAL_PORT,REMOTE_PORT);
+	#else
+		sprintf(cmd, "./adb-linux forward tcp:%d tcp:%d", LOCAL_PORT,REMOTE_PORT);
+	#endif
+	ret = system(cmd);
+#endif
+
 	if(ret)
 		return 0;
     return 1;
